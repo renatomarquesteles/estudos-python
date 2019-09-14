83f5563edb5690c2012f21c47d7c3137fc50f0fe
Renato <renatomarquesteles@gmail.com>
2019-09-14 00:58:11 -0300

    curso em video exs 101 a 105

---
+++ Exercicios Curso em Video/ex104.py
@@ -0,0 +1,17 @@
+def leia_int(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = input(msg)
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+n = leia_int('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
