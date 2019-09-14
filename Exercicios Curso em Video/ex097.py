59b9d73ae22f2c57998c07ada6f350ff3bf8a24f
Renato <renatomarquesteles@gmail.com>
2019-09-14 00:43:02 -0300

    curso em video exs 96 a 100

---
+++ Exercicios Curso em Video/ex097.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f' {msg}')
+    print('~' * tam)
+
+
+escreva('Renato Marques')
+escreva('Curso de Python')
+escreva('CeV')
