c87d0ea1de3528941c659d9be1607adff461247b
Renato <renatomarquesteles@gmail.com>
2019-09-29 16:38:14 -0300

    hacker rank: staircase

---
+++ HackerRank/Problem Solving/Staircase.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(n):
+        print(f'{(i+1)*"#":>{n}}')
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
